4a0584684f8d73e5802bef0a800c7282825d706b
GutoAdm <cesaraugusto.gaiotto@gmail.com>
2024-10-25 00:03:07 -0300

    criando o package service

--- src/main/java/com/musicas/ctr/MusicaController.java
+++ src/main/java/com/musicas/ctr/MusicaController.java
@@ -1,6 +1,8 @@
 package com.musicas.ctr;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,10 @@ public class MusicaController {
 		Musica m = new Musica();
 		m.setAutor("Sou o César autor disto aqui");
 		return m.getAutor();
+	}
+	
+	@PostMapping("add-teste")
+	public String addMusica(@RequestBody Musica musica) {
+		return null;
 	}
 }
---
+++ src/main/java/com/musicas/svr/MusicaService.java
@@ -0,0 +1,16 @@
+package com.musicas.svr;
+
+import org.springframework.stereotype.Service;
+
+//import com.musicas.mdl.Musica;
+
+@Service
+public class MusicaService {
+
+	//public Musica createMusica(Musica musica) {
+		
+//		Musica msc_create = mu
+//		
+//		return ;
+	}
+//}
